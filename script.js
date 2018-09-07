df7cbf40103e132490304c74496891acdc01b9ef
Jasper O'Leary <jhaazpr@gmail.com>
2018-09-06 18:46:25 -0700

    commentary

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**
+ * Removes iframe in Chrome to prevent two songs from playing
+ */
 $(document).ready(() => {
   let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
   if (!isChrome) {
